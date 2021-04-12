337275eecba07281f274158281a72bd646ec67e5
Jonathan Manousian <jonathanmanousian@Jonathans-Air.lan>
2021-04-12 18:44:04 +0200

    badly organized groups

--- src/views/explore/Explore.js
+++ src/views/explore/Explore.js
@@ -1,3 +1,10 @@
+import { Group } from './Groups/Groups'
+
 export const Explore = () => {
-  return <div>Time to explore some videos!</div>
+  return (
+    <div>
+      Time to explore some videos!
+      <Group />
+    </div>)
+
 }
