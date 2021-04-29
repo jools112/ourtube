595690c666391f54eee5f79e973fd794cd7637a7
Inomares <f-a-k-i-e@hotmail.com>
2021-04-29 19:25:33 +0200

    path attempt #2

---
+++ src/views/Explore_fix/Explore.js
@@ -0,0 +1,13 @@
+import './Explore.css'
+import { Group } from './Groups/Groups'
+import { SoftBox } from '../../components/SoftBox'
+import { Button } from '../../components/Button'
+import { TextField } from '../../components/TextField'
+
+export const Explore = () => {
+  return (
+    <div className="Explore">
+      <Group />
+    </div>
+  )
+}
---
+++ src/views/Explore_fix/Groups/Groups.js
@@ -0,0 +1,99 @@
+import './Groups.css'
+// eslint-disable-next-line
+import firebase from '../../../firebase'
+import React, { useState } from 'react'
+import { useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
+import { Button } from '../../../components/Button'
+import { TextField } from '../../../components/TextField'
+import { SoftBox } from '../../../components/SoftBox'
+
+export const Group = () => {
+  const [groups, setGroups] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [title, setTitle] = useState('')
+  const [data, setData] = useState('')
+
+  const ref = firebase.firestore().collection('groups')
+
+  function getSchools() {
+    setLoading(true)
+    ref.onSnapshot((querySnapshot) => {
+      const items = []
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data())
+      })
+      setGroups(items)
+      setLoading(false)
+    })
+  }
+
+  function groupInfo(data) {
+    document.getElementById('groupInfo').innerHTML = data
+  }
+
+  useEffect(() => {
+    getSchools()
+  }, [])
+
+  // ADD FUNCTION
+  function addGroup(newGroup) {
+    ref
+      .doc(newGroup.id)
+      .set(newGroup)
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
+  if (loading) {
+    return <h1 className="GroupsLoading">Loading...</h1>
+  }
+
+  return (
+    <div>
+      <div>
+        <div className="GroupsSubmitContainer">
+          <TextField
+            label="title:"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          ></TextField>
+          <br></br>
+          <TextField
+            label="data:"
+            value={data}
+            onChange={(e) => setData(e.target.value)}
+          ></TextField>
+          <br />
+          <div>
+            <Button onClick={() => addGroup({ title, data, id: uuidv4() })}>
+              Submit
+            </Button>
+          </div>
+        </div>
+        <br />
+        <div>
+          <Button>Create</Button>
+        </div>
+      </div>
+      <div>
+        <br />
+        <SoftBox
+          title="GROUPS"
+          content={groups.map((group) => (
+            <div className="GroupsButton">
+              <Button key={group.id} onClick={() => groupInfo(group.data)}>
+                {group.title}
+              </Button>
+            </div>
+          ))}
+        ></SoftBox>
+        <br />
+      </div>
+
+      <div id="groupInfo"></div>
+    </div>
+  )
+}
