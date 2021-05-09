aadc5d0bd400f126cbaa2a5562738e7a5103e554
Daniela Marin <dnmm@kth.se>
2021-05-09 17:01:58 +0200

    changed host

--- src/views/Watch/VideoPlayer/VideoPlayer.js
+++ src/views/Watch/VideoPlayer/VideoPlayer.js
@@ -43,9 +43,9 @@ const UnconnectedVideoPlayer = (props) => {
     if (!props.newStateUserName) {
       props.dispatchUserNameActionCreator(readCookie('session'))
     }
-    conn = new WebSocket('ws://localhost:3000/test')
+    //  conn = new WebSocket('ws://localhost:3000/test')
 
-    //conn = new WebSocket('ws://193.122.13.192:3000/test')
+    conn = new WebSocket('ws://193.122.13.192:3000/test')
     conn.onmessage = function (ev) {
       var matches
       console.log(ev, ev.data)
