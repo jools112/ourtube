5670df3f62d2e2c2dd775b4a5413852edfac213b
Daniela Marin <dnmm@kth.se>
2021-05-17 17:44:33 +0200

    added script file type

--- src/views/Watch/VideoPlayer/VideoPlayer.js
+++ src/views/Watch/VideoPlayer/VideoPlayer.js
@@ -40,6 +40,7 @@ const UnconnectedVideoPlayer = (props) => {
     const scriptHtml5 = document.createElement('script')
     scriptHtml5.src = 'html5-youtube.js'
     scriptHtml5.async = false
+    scriptHtml5.type = 'jsx'
     document.body.appendChild(scriptHtml5)
     const scriptYoutube = document.createElement('script')
     scriptYoutube.src = 'https://www.youtube.com/iframe_api'
